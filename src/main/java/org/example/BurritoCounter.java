56a5d796901e45ffbd0504980a941d570b7f4e77
Rochelle Charles <roccharl@amazon.co.uk>
2024-04-18 07:48:00 +0100

    BurritoCounter class for loop example

---
+++ src/main/java/org/example/BurritoCounter.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class BurritoCounter {
+    public static int getBurritosSold(int lastOrderNumber) {
+        int totalBurritosSold = 0;
+
+        for(int i = 0; i <= lastOrderNumber; i++){
+            totalBurritosSold +=i;
+        }
+        return totalBurritosSold;
+    }
+
+    public static void main(String []args) {
+        // Try testing your code with different arguments in the method call
+        System.out.println(getBurritosSold(548));
+    }
+}
